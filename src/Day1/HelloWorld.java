c0e582e130ba2284400ee8014ba9135849f0a9a5
Zalina <Zalinasoria@gmail.com>
2021-04-24 16:56:22 -0400

    added more work1

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hi");
         System.out.println("Stuff on git");
+        //adding final wrap up code
     }
 }
