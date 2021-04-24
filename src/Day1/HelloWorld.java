5b0064b8a639f6eeb4b5270f93cbdf10bcf48e4d
Zalina <Zalinasoria@gmail.com>
2021-04-24 16:52:45 -0400

    added more work

---
+++ src/Day1/HelloWorld.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hi");
+        System.out.println("Stuff on git");
+    }
+}
